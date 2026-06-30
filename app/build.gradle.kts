7140e6869d84ef7f5e364cad92d1dfc39c0f5f76
aNaruto <r.angka.kusuma@gmail.com>
2026-07-01 02:17:02 +0800

    build: configure abiFilters to output arm64-v8a APK only

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,9 +90,9 @@ android {
     splits {
         abi {
             isEnable = true
-            isUniversalApk = true
+            isUniversalApk = false
             reset()
-            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+            include("arm64-v8a")
         }
     }
 
